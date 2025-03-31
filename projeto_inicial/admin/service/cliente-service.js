9e5ae4497deebe00aa700d0e21f8301d59a2e32e
Alexis1Gomez <ajgrramos5@gmail.com>
2025-03-30 22:47:27 -0400

    avanze curso JS CRUD

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,33 +1,42 @@
-const criaNovaLinha = (nome, email) => {
-     const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                    <td>${email}</td>
-                    <td>
-                        <ul class="tabela__botoes-controle">
-                            <li><a href="./telas/edita_cliente.
-                            html" class="botao-simples botao-simples--editar">Editar</a></li>
-                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                        </ul>
-    </td> 
-    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
+const listaCliente = () => {
+  return fetch('http://localhost:3000/profile')
+  .then(resposta => {
+    return resposta.json()
+  })
 }
 
-const tabela = document.querySelector('[data-tabela]')
+const criaCliente = (nome,email) => {
+    return fetch (`http://localhost:3000/profile`, {
+        method : 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email : email
+        })
+    })
+    .then( resposta => {
+        return resposta.json()
+    })
+}
 
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
 
+const detalhaCliente  = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+  .then(resposta => {
+    return resposta.json()
+  })
+}
 
-const http = new XMLHttpRequest()
-
-http.open('GET', 'http://localhost:3000/profile')
-
-http.send()
-
-http.onload = () => {
-    const data = JSON.parse(http.response)
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))
-    });
+export const clienteService = {
+    listaCliente,
+    criaCliente,
+    removeCliente,
+    detalhaCliente
 }
